38d324ad8a3b59a495c15e10baaa015786cdb4fe
Jaques Claudino <jaques.claudino@gmail.com>
2018-07-12 18:02:49 -0300

    12/07/2018 - nxfaces 0.0.19 - websocket improvements

--- src/main/resources/META-INF/resources/js/nxwebsocket.js
+++ src/main/resources/META-INF/resources/js/nxwebsocket.js
@@ -1,31 +1,48 @@
-function NxWebSocket(host, contextPath, websocketName, log, showLog, onmessage) {
-    this.log = log;
-    this.showLog = showLog;
+function NxWebSocket(host, contextPath, websocketName, logVisible, logOnlyError, onmessage) {
+    this.logVisible = logVisible;
+    this.logOnlyError = logOnlyError;
     this.onmessage = onmessage;
+    connected = false;
+    connectMessage = null;
 
+    ws = createWebSocket();
+    if (ws !== null) {
+        ws.onopen = function(evt) {
+            addLog('Connected', false);     
+            connected = true;
+
+            if (connectMessage !== null) {
+                ws.send(connectMessage);
+            }
+        };
+
+        ws.onclose = function(evt) {
+            addLog('Disconnected', true);
+            connected = false;
+        };
+
+        ws.onmessage = function(evt) {
+            addLog('Received: ' + evt.data, false);
+            if (onmessage !== null) {
+                onmessage(evt.data);
+            }
+        };
+
+        ws.onerror = function(evt) {
+            addLog('Error: ' + evt.data, true);
+        };
+    }
+    
     this.setOnMessage = function(onmessage) {
         this.onmessage = onmessage;
     };
-
-    ws = createWebSocket();
-    ws.onopen = function(evt) {
-        addLog('Connected');
-        //ws.send("test client connected")
-    };
-
-    ws.onclose = function(evt) {
-        addLog('Disconnected');
-    };
-
-    ws.onmessage = function(evt) {
-        addLog('Received: ' + evt.data);
-        if (onmessage !== null) {
-            onmessage(evt.data);
+    
+    this.sendMessage = function(message) {
+        if (connected) {
+            ws.send(message);
+        } else {
+            connectMessage = message;
         }
-    };
-
-    ws.onerror = function(evt) {
-        addLog('Error: ' + evt.data);
     };
 
     function createWebSocket() {
@@ -50,30 +67,34 @@ function NxWebSocket(host, contextPath, websocketName, log, showLog, onmessage) 
         } else if ('MozWebSocket' in window) {
             return new MozWebSocket(url);
         } else {
-            addLog('Browser not supported.');
+            addLog('Browser not supported.', true);
             return null;
         }
     }
 
-    function addLog(message) {
-        if (!showLog) {
+    function addLog(message, isError) {
+        if (!logVisible) {
             return;
         }
 
-        now = new Date();
-        fmtDate =
-                formatField(now.getHours(), 2) + ':' +
-                formatField(now.getMinutes(), 2) + ':' +
-                formatField(now.getSeconds(), 2) + ':' +
-                formatField(now.getMilliseconds(), 3);
+        line = 'WebSocket: ' + message;
+        log = document.getElementById("log");
+        if (log !== null && typeof log !== 'undefined' && (isError || !logOnlyError)) {
+            setTimeout(function(){ 
+                log.innerHTML = line;
+            }, 2000);
+        }
 
-        line = fmtDate + ' WebSocket: ' + message;
         if (typeof console !== 'undefined') {
-            console.log(line);
-        }
-        if (log !== null) {
-            log.innerHTML = line;
-        }
+            now = new Date();
+            fmtDate =
+                    formatField(now.getHours(), 2) + ':' +
+                    formatField(now.getMinutes(), 2) + ':' +
+                    formatField(now.getSeconds(), 2) + '.' +
+                    formatField(now.getMilliseconds(), 3);
+            
+            console.log(fmtDate + ' ' + line);
+        }        
     }
 
     function formatField(value, len) {
